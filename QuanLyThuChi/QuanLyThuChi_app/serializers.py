079f1e9bae9934f3145f1da64481e785ba7ea339
huythua <110289075+huythua@users.noreply.github.com>
2024-05-19 11:59:17 +0700

    update
    .

--- QuanLyThuChi/QuanLyThuChi_app/serializers.py
+++ QuanLyThuChi/QuanLyThuChi_app/serializers.py
@@ -6,7 +6,6 @@ from .models import *
 class UserSerializer(ModelSerializer):
     class Meta:
         model = User
-        fields = ['id', 'first_name', 'last_name', 'email', 'username', 'password', 'avatar']
         fields = ['id', 'first_name', 'last_name', 'email', 'username', 'password', 'avatar', 'account_type']
         extra_kwargs = {
             'password': {
